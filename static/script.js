68ea735bbb760e234a83e7bede3412767050007b
Rishi Kumar <rsi.sxs@gmail.com>
2022-09-27 22:35:39 +0530

    add hero-section

--- static/script.js
+++ static/script.js
@@ -1,3 +1,36 @@
+const marquee = document.querySelectorAll(".clipped-text");
+const gdsc = document.querySelectorAll(".gdsc");
+// const tl = gsap.timeline({ default: { duration: 0.75, ease: "power1.out" } });
+
+// tl.fromTo(".gdsc", { scale: 0 }, { scale: 1, duration: 1.5 });
+// console.log(marquee);
+// added event listener because it doesn't get the right width
+addEventListener("load", function () {
+  marquee.forEach((el) => {
+    console.log(el);
+    // set a default rate, the higher the value, the faster it is
+    let rate = 200;
+    // get the width of the element
+    let distance = el.clientWidth;
+    // get the margin-right of the element
+    let style = window.getComputedStyle(el);
+    let marginRight = parseInt(style.marginRight) || 0;
+    // get the total width of the element
+    let totalDistance = distance + marginRight;
+    // get the duration of the animation
+    // for a better explanation, see the quoted codepen in the first comment
+    let time = totalDistance / rate;
+    // get the parent of the element
+    let container = el.parentElement;
+
+    tl.to(container, time, {
+      repeat: -1,
+      x: "-" + totalDistance,
+      ease: Linear.easeNone,
+    });
+  });
+});
+
 const rbinary = document.querySelector(".rbinary");
 const randomBinary = [
   "11100100",
@@ -69,7 +102,7 @@ const interval = setInterval(function () {
   arr.forEach((e) => {
     // console.log(e);
     const r = randomBinary[e].split("");
-    console.log(r);
+    // console.log(r);
     r.forEach((z) => {
       str += z;
     });
@@ -77,5 +110,16 @@ const interval = setInterval(function () {
   });
   rbinary.textContent = str;
 }, 1000);
-
 // clearInterval(interval); // thanks @Luca D'Amico
+// const tl = gsap.timeline({ default: { duration: 0.75, ease: "power1.out" } });
+// tl.fromTo(
+//   ".marquee_content_1",
+//   { x: 0 },
+//   { x: -100, ease: Linear.easeNone, duration: 1.5, yoyo: true }
+// );
+
+// https://codepen.io/lelamanolio/pen/ZEYRJJJ
+// Inspired by:
+// https://codepen.io/sgorneau/pen/mWEddP
+// as described in the above codepen, if we set the value `x` to something like -100% it'll go too fast
+// so I took the function to calculate the rate and applied to my code
